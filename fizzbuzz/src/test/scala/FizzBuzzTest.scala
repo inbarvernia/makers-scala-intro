0b9824a36b0dedc75a62556028dd74e4f3773ba8
Inbar Vernia <inbar.vernia@gmail.com>
2022-03-15 17:44:41 +0000

    Typed out starter test and class files for FizzBuzz

---
+++ fizzbuzz/src/test/scala/FizzBuzzTest.scala
@@ -0,0 +1,11 @@
+import org.scalatest.funcuite.AnyFunSuite
+
+class FizzBuzzTest extends AnyFunSuite {
+  test("lists the numbers up to one") {
+    assert(FizzBuzz.generate(1) === "1")
+  }
+
+  test("lists the numbers up to two") {
+    assert(FizzBuzz.generate(2) === "1, 2")
+  }
+}
