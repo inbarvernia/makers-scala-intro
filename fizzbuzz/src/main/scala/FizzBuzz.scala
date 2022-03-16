a89d1607c8a8c821c38b7aa6ccd3d3b3ff188f27
Inbar Vernia <inbar.vernia@gmail.com>
2022-03-16 17:14:18 +0000

    Refactored FizzBuzz by composing val functiones using andThen

--- fizzbuzz/src/main/scala/FizzBuzz.scala
+++ fizzbuzz/src/main/scala/FizzBuzz.scala
@@ -3,33 +3,27 @@ import scala.collection.mutable.ArrayBuffer
 object FizzBuzz {
   def generate(number: Int): String = {
 
-    val nums = createArrayBufferOneToNumber(number)
-
-    val fizzedNums: ArrayBuffer[String] = mapToFizzBuzz(nums)
-
-    val result = convertListToString(fizzedNums)
-
-    return result;
+    (createIntArrayUpToNumber andThen mapToFizzBuzz andThen convertListToString)(number)
   }
 
-  def createArrayBufferOneToNumber(num: Int): ArrayBuffer[Int] = {
+  val createIntArrayUpToNumber = (num: Int) => {
     val nums = ArrayBuffer[Int]()
 
     for (i <- 1 to num) {
       nums += i
     }
-    return nums
+    nums
   }
 
-  def fizzBuzzify(num: Int): String = {
+  val fizzBuzzify = (num: Int) => {
     if (num % 15 == 0) "FizzBuzz" else if (num % 3 == 0) "Fizz" else if (num % 5 == 0) "Buzz" else num.toString()
   }
 
-  def mapToFizzBuzz(arr: ArrayBuffer[Int]) : ArrayBuffer[String] = {
+  val mapToFizzBuzz = (arr: ArrayBuffer[Int]) => {
     arr.map(fizzBuzzify)
   }
 
-  def convertListToString(arr: ArrayBuffer[String]): String = {
+  val convertListToString = (arr: ArrayBuffer[String]) => {
     arr.mkString(", ")
   }
 }
