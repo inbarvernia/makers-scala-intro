03bf18d60c58368b6d46cb14fb3430ef2d172c2f
Inbar Vernia <inbar.vernia@gmail.com>
2022-03-16 15:42:22 +0000

    Refactored FizzBuzz: extracted fizzBuzzify support function

--- fizzbuzz/src/main/scala/FizzBuzz.scala
+++ fizzbuzz/src/main/scala/FizzBuzz.scala
@@ -5,19 +5,23 @@ object FizzBuzz {
 
     val nums = createArrayBufferOneToNumber(number)
 
-    val fizzedNums: ArrayBuffer[String] = nums.map(num => if (num % 15 == 0) "FizzBuzz" else if (num % 3 == 0) "Fizz" else if (num % 5 == 0) "Buzz" else num.toString())
+    val fizzedNums: ArrayBuffer[String] = nums.map(fizzBuzzify)
 
     val result = fizzedNums.mkString(", ")
 
     return result;
   }
 
-  def createArrayBufferOneToNumber(number: Int): ArrayBuffer[Int] = {
+  def createArrayBufferOneToNumber(num: Int): ArrayBuffer[Int] = {
     val nums = ArrayBuffer[Int]()
 
-    for (i <- 1 to number) {
+    for (i <- 1 to num) {
       nums += i
     }
     return nums
+  }
+
+  def fizzBuzzify(num: Int): String = {
+    if (num % 15 == 0) "FizzBuzz" else if (num % 3 == 0) "Fizz" else if (num % 5 == 0) "Buzz" else num.toString()
   }
 }
