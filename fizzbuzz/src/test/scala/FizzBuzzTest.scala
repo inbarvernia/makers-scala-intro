d9c47c9c5c40e6c0e9d19323a64a0bc61ee9bef3
Inbar Vernia <inbar.vernia@gmail.com>
2022-03-16 15:15:18 +0000

    Added failing test for FizzBuzz.generate(30)

--- fizzbuzz/src/test/scala/FizzBuzzTest.scala
+++ fizzbuzz/src/test/scala/FizzBuzzTest.scala
@@ -25,7 +25,11 @@ class FizzBuzzTest extends AnyFunSuite {
     assert(FizzBuzz.generate(10) === "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz")
   }
 
-  test("replaces multiples of three AND five with FizzBuzz") {
+  test("replaces fifteen with FizzBuzz") {
     assert(FizzBuzz.generate(15) === "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz")
+  }
+
+  test("replaces multiples of fifteen with FizzBuzz") {
+    assert(FizzBuzz.generate(30) ===  "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz")
   }
 }
