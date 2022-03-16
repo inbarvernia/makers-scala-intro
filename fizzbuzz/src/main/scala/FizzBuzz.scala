9621af869beab87d02423bcb9069ea9b7f7b1b4b
Inbar Vernia <inbar.vernia@gmail.com>
2022-03-16 15:40:01 +0000

    Refactored FizzBuzz: extracted createArrayBufferOneToNumber support function

--- fizzbuzz/src/main/scala/FizzBuzz.scala
+++ fizzbuzz/src/main/scala/FizzBuzz.scala
@@ -3,16 +3,21 @@ import scala.collection.mutable.ArrayBuffer
 object FizzBuzz {
   def generate(number: Int): String = {
 
-    val nums = ArrayBuffer[Int]()
-
-    for (i <- 1 to number) {
-      nums += i
-    }
+    val nums = createArrayBufferOneToNumber(number)
 
     val fizzedNums: ArrayBuffer[String] = nums.map(num => if (num % 15 == 0) "FizzBuzz" else if (num % 3 == 0) "Fizz" else if (num % 5 == 0) "Buzz" else num.toString())
 
     val result = fizzedNums.mkString(", ")
 
     return result;
+  }
+
+  def createArrayBufferOneToNumber(number: Int): ArrayBuffer[Int] = {
+    val nums = ArrayBuffer[Int]()
+
+    for (i <- 1 to number) {
+      nums += i
+    }
+    return nums
   }
 }
