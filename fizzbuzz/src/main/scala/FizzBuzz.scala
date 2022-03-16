3dfde19c2195cdc588216b39900361db77e66e5a
Inbar Vernia <inbar.vernia@gmail.com>
2022-03-16 15:51:34 +0000

    Refactored FizzBuzz: extracted mapToFizzBuzz support function

--- fizzbuzz/src/main/scala/FizzBuzz.scala
+++ fizzbuzz/src/main/scala/FizzBuzz.scala
@@ -5,7 +5,7 @@ object FizzBuzz {
 
     val nums = createArrayBufferOneToNumber(number)
 
-    val fizzedNums: ArrayBuffer[String] = nums.map(fizzBuzzify)
+    val fizzedNums: ArrayBuffer[String] = mapToFizzBuzz(nums)
 
     val result = convertListToString(fizzedNums)
 
@@ -23,6 +23,10 @@ object FizzBuzz {
 
   def fizzBuzzify(num: Int): String = {
     if (num % 15 == 0) "FizzBuzz" else if (num % 3 == 0) "Fizz" else if (num % 5 == 0) "Buzz" else num.toString()
+  }
+
+  def mapToFizzBuzz(arr: ArrayBuffer[Int]) : ArrayBuffer[String] = {
+    arr.map(fizzBuzzify)
   }
 
   def convertListToString(arr: ArrayBuffer[String]): String = {
