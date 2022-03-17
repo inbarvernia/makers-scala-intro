6d84864131d98bce01889287e74aae4da6614e25
Inbar Vernia <inbar.vernia@gmail.com>
2022-03-17 09:40:31 +0000

    Refactored FizzBuzz createIntArray using toBuffer

--- fizzbuzz/src/main/scala/FizzBuzz.scala
+++ fizzbuzz/src/main/scala/FizzBuzz.scala
@@ -7,23 +7,18 @@ object FizzBuzz {
   }
 
   val createIntArrayUpToNumber = (num: Int) => {
-    val nums = ArrayBuffer[Int]()
-
-    for (i <- 1 to num) {
-      nums += i
-    }
-    nums
+    (1 to num).toBuffer
   }
 
   val fizzBuzzify = (num: Int) => {
     if (num % 15 == 0) "FizzBuzz" else if (num % 3 == 0) "Fizz" else if (num % 5 == 0) "Buzz" else num.toString()
   }
 
-  val mapToFizzBuzz = (arr: ArrayBuffer[Int]) => {
+  val mapToFizzBuzz = (arr: Iterable[Int]) => {
     arr.map(fizzBuzzify)
   }
 
-  val convertListToString = (arr: ArrayBuffer[String]) => {
+  val convertListToString = (arr: Iterable[String]) => {
     arr.mkString(", ")
   }
 }
