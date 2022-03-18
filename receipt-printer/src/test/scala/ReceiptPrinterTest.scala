4edc1cae3008ddfd33e200d166d82e39b014a6f2
Inbar Vernia <inbar.vernia@gmail.com>
2022-03-18 10:29:50 +0000

    Receipt printer: passed test for displaying address

--- receipt-printer/src/test/scala/ReceiptPrinterTest.scala
+++ receipt-printer/src/test/scala/ReceiptPrinterTest.scala
@@ -34,7 +34,13 @@ class ReceiptPrinterSpec extends AnyWordSpec with Matchers {
         )
         printer.receipt should include ("The Coffee Connection")
       }
-      // add more tests here.
+      "contains the address of the cafe" in {
+        val printer = new ReceiptPrinter(
+          coffeeConnectionCafe,
+          Map("Cafe Latte" -> 1)
+        )
+        printer.receipt should include ("123 Lakeside Way")
+      }
     }
   }
 }
